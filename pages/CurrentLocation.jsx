c6196d2f7680835822353bc170e0d37a8704fbd6
mohammadakil <mohammadakil.mobileappdev@gmail.com>
2023-08-31 17:19:42 +0530

    adding distance finding fun

--- pages/CurrentLocation.jsx
+++ pages/CurrentLocation.jsx
@@ -9,13 +9,70 @@ import {
 import React, {useEffect, useState} from 'react';
 import Geolocation from '@react-native-community/geolocation';
 import {request, PERMISSIONS} from 'react-native-permissions';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker, Polyline} from 'react-native-maps';
+import {getDistance} from 'geolib';
+import Toast from 'react-native-toast-message';
+
 const CurrentLocation = () => {
+  const [markers, setMarkers] = useState([]);
   const [location, setlocation] = useState({
     lat: 0,
     lon: 0,
   });
-  console.log(location);
+
+  const handleMapPress = event => {
+    const newMarkers = [...markers];
+    newMarkers.push({
+      id: newMarkers.length + 1,
+      coordinate: event.nativeEvent.coordinate,
+    });
+    setMarkers(newMarkers);
+  };
+
+  const [secondPosition, setSecondPosition] = useState({
+    latitude: 23.0225,
+    longitude: 72.5714,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
+
+  const [firstPosition, setFirstPosition] = useState({
+    latitude: location.lat,
+    longitude: location.lon,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
+
+  const markerHandler = cords => {
+    setSecondPosition({
+      latitude: cords.coordinate.latitude,
+      longitude: cords.coordinate.longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    });
+
+    setSecondPosition(prev => {
+      return {
+        latitude: cords.coordinate.latitude,
+        longitude: cords.coordinate.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      };
+    });
+  };
+
+  const findDistance = () => {
+    let distance = getDistance(
+      {latitude: firstPosition.latitude, longitude: firstPosition.longitude},
+      {latitude: secondPosition.latitude, longitude: secondPosition.longitude},
+    );
+    console.log(typeof distance);
+    Toast.show({
+      visibilityTime: 2000,
+      type: 'success',
+      text1: 'Distance is ' + distance.toString() + ' meteres',
+    });
+  };
   const fetchGeolocation = async () => {
     try {
       if (Platform.OS == 'android') {
@@ -25,8 +82,12 @@ const CurrentLocation = () => {
         if (permissionResult === 'granted') {
           Geolocation.getCurrentPosition(
             position => {
-              console.log(position.coords.latitude);
-              console.log(position.coords.longitude);
+              setFirstPosition({
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+              });
               setlocation(prev => {
                 return {
                   ...prev,
@@ -89,8 +150,25 @@ const CurrentLocation = () => {
           showsUserLocation
           showsCompass
           followsUserLocation
-          style={{width: '100%', height: '90%'}}
-        />
+          onPress={handleMapPress}
+          style={{width: '100%', height: '80%'}}>
+          {markers.map(marker => (
+            <Marker
+              key={marker.id}
+              coordinate={{
+                latitude: marker.coordinate.latitude,
+                longitude: marker.coordinate.longitude,
+              }}
+              onPress={() => markerHandler(marker)}
+            />
+          ))}
+          <Polyline
+            coordinates={[firstPosition, secondPosition]} //specify our coordinates
+            strokeColor={'#000'}
+            strokeWidth={3}
+            lineDashPattern={[1]}
+          />
+        </MapView>
 
         <TouchableOpacity
           style={styles.button}
@@ -98,6 +176,10 @@ const CurrentLocation = () => {
             fetchGeolocation();
           }}>
           <Text style={styles.buttonText}>Get current location</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={findDistance}>
+          <Text style={styles.buttonText}>Get Distance</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
